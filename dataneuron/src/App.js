c4022c2737e6aee25fbe7f8cd66b6876327e1244
Shivani-Banduni <shivanibanduni009@gmail.com>
2024-02-02 11:26:40 +0530

    grid layout part done

---
+++ dataneuron/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import GridLayout from 'react-grid-layout';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+import './App.css'; // For any additional custom styling
+import Gridlayout from './components/Gridlayout';
+
+function App() {
+ 
+
+  return (
+  <>
+<Gridlayout/>
+  </>
+  );
+}
+
+export default App;
